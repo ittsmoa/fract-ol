a416e1365ad1b4aa34f2410bae8d68ce20932c52
Mohammed Zaher <mohammedzaher9087@gmail.com>
2026-02-01 17:24:37 +0300

    dc

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: moatieh <moatieh@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 18:26:20 by moatieh           #+#    #+#             */
-/*   Updated: 2026/02/01 16:00:17 by moatieh          ###   ########.fr       */
+/*   Updated: 2026/02/01 17:09:47 by moatieh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,33 @@ static int	same_str(char *a, char *b)
 	return (0);
 }
 
+static int	is_valid_double(char *s)
+{
+	int	has_digit;
+	int	has_dot;
+
+	has_digit = 0;
+	has_dot = 0;
+	if (*s == '-' || *s == '+')
+		s++;
+	while (*s)
+	{
+		if (*s >= '0' && *s <= '9')
+			has_digit = 1;
+		else if (*s == '.' && !has_dot)
+			has_dot = 1;
+		else
+			return (0);
+		s++;
+	}
+	return (has_digit);
+}
+
 static int	set_mode(t_app *app, int ac, char **av)
 {
 	if (ac < 2)
 		return (0);
-	if (same_str(av[1], "mandelbrot"))
+	if (same_str(av[1], "mandelbrot") && ac == 2)
 	{
 		app->mode = MODE_MANDEL;
 		app->c_re = 0.0;
@@ -37,6 +59,8 @@ static int	set_mode(t_app *app, int ac, char **av)
 	}
 	if (ac == 4 && same_str(av[1], "julia"))
 	{
+		if (!is_valid_double(av[2]) || !is_valid_double(av[3]))
+			return (0);
 		app->mode = MODE_JULIA;
 		app->c_re = ft_atod(av[2]);
 		app->c_im = ft_atod(av[3]);
@@ -48,8 +72,13 @@ static int	set_mode(t_app *app, int ac, char **av)
 static void	show_usage(void)
 {
 	write(1, "Usage:\n", 7);
-	write(1, "./fractol mandelbrot\n", 21);
-	write(1, "./fractol julia <re> <im>\n", 26);
+	write(1, "  ./fractol mandelbrot\n", 23);
+	write(1, "  ./fractol julia <real> <imaginary>\n", 38);
+	write(1, "\nExamples:\n", 11);
+	write(1, "  ./fractol mandelbrot\n", 23);
+	write(1, "  ./fractol julia -0.7 0.27015\n", 31);
+	write(1, "  ./fractol julia -0.8 0.156\n", 29);
+	write(1, "  ./fractol julia 0.285 0.01\n", 29);
 }
 
 int	main(int ac, char **av)
